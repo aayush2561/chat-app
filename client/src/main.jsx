439f69faccb01955b5cca71921258bb98e9197ab
aayush2561 <143727610+aayush2561@users.noreply.github.com>
2024-08-26 14:41:49 +0545

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'react-toastify/dist/ReactToastify.css'; 
 import { ToastContainer } from 'react-toastify';
 import { AuthContextProvider } from './context/AuthContext.jsx';
-import { SocketContextProvider } from './context/SocketContext.jsX';
+import { SocketContextProvider } from './context/SocketContext.jsx';
 
 
 createRoot(document.getElementById('root')).render(
