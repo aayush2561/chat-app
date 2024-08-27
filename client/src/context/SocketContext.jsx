32aa06c0a811349dfd66346a58b1136861e504e5
aayush2561 <143727610+aayush2561@users.noreply.github.com>
2024-08-27 14:25:03 +0545

    Update SocketContext.jsx

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({children }) => {
     const {authUser} =useAuthcontext();
     useEffect(() => {
         if(authUser){
-            const socket=io("http://localhost:3000",{
+            const socket=io(import.meta.env.VITE_API_URL,{
             query:{
                 userId:authUser._id,
             }
